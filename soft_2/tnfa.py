ef0c7b86eeef19c45d0003b56121b8d345471fe5
0dminnimda <0dminnimda@gmail.com>
2024-04-10 17:28:32 +0300

    soft_2\tnfa.py: comment out unused code

--- soft_2/tnfa.py
+++ soft_2/tnfa.py
@@ -111,12 +111,12 @@ class TNFA(Generic[E]):
         path = self.dump_dot(path)
         os.system(f"dot -Tpng -Gdpi=300 {path} -o {path}.png")  # coz why not
 
-    def get_mapped_symbol_transitions(self) -> dict[tuple[State, E], State]:
-        mapped_sym = {}
-        for q, s, p in self.symbol_transitions:
-            val = mapped_sym.get((q, s), set())
-            mapped_sym[(q, s)] = val | {p}
-        return mapped_sym
+    # def get_mapped_symbol_transitions(self) -> dict[tuple[State, E], State]:
+    #     mapped_sym = {}
+    #     for q, s, p in self.symbol_transitions:
+    #         val = mapped_sym.get((q, s), set())
+    #         mapped_sym[(q, s)] = val | {p}
+    #     return mapped_sym
 
     def get_double_mapped_symbol_transitions(self) -> DblMapSymTrans:
         mapped_sym = {}
