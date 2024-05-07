2efbb294d4cf26144071f6dc40dacd00d8bb9fb4
0dminnimda <0dminnimda@gmail.com>
2024-05-07 19:06:16 +0300

    soft_2\test.py: remove the need in truncating the groups

--- soft_2/test.py
+++ soft_2/test.py
@@ -63,13 +63,9 @@ def test_one_regex(regex, cases):
                         f"{prompt!r} did not match but expected groups captured for {regex!r} [tDfa]"
                     )
             else:
-                trankated_groups = {
-                    name: ([None] + [it for it in capture if it is not None])[-1:]
-                    for name, capture in groups.items()
-                }
-                if trankated_groups != match_tdfa:
+                if groups != match_tdfa:
                     _reported.append(
-                        f"{prompt!r} expected to match groups {trankated_groups}, but got {match_tdfa} for {regex!r} [tDfa]"
+                        f"{prompt!r} expected to match groups {groups}, but got {match_tdfa} for {regex!r} [tDfa]"
                     )
 
 
